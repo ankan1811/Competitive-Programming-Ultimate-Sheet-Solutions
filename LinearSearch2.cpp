19a4c9067c81e0ff8a846eb650fecb679011acb3
Ankan Pal <ankan1811@gmail.com>
2021-01-29 17:49:43 +0530

    Add files via upload

---
+++ LinearSearch2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  // freopen("TASK.in", "r", stdin);freopen("TASK.out", "w", stdout);
+  int n;
+  cin >> n;
+  long long sum = 0;
+  int cnt = 0;
+  int mx = -2e9;
+  for(int i = 1; i <= n; i++) {
+    int x;
+    scanf("%d", &x);
+    if(x >= 0) sum += 1LL * x, cnt++;
+    mx = max(mx, x);
+  }
+  if(cnt) cout << sum << " " << cnt << endl;
+  else cout << mx << " " << 1 << endl;
+  return 0;
+}
